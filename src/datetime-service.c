47f45a55af573d7e46b8743bd8162118ba23b88d
Alex Chiang <achiang@canonical.com>
2011-06-14 10:33:10 -0600

    src/datetime-service.c: force calendar to always show (LP: #793508)

--- src/datetime-service.c
+++ src/datetime-service.c
@@ -93,10 +93,6 @@ check_for_calendar (gpointer user_data)
 {
 	g_return_val_if_fail (calendar != NULL, FALSE);
 
-	// Always enable the calendar even if it does nothing
-	dbusmenu_menuitem_property_set_bool(calendar, DBUSMENU_MENUITEM_PROP_ENABLED, TRUE);
-	dbusmenu_menuitem_property_set_bool(calendar, DBUSMENU_MENUITEM_PROP_VISIBLE, TRUE);
-
 	gchar *evo = g_find_program_in_path("evolution");
 	if (evo != NULL) {
 		g_debug("Found the calendar application: %s", evo);
@@ -148,13 +144,12 @@ build_menus (DbusmenuMenuitem * root)
 	if (calendar == NULL) {
 		calendar = dbusmenu_menuitem_new();
 		dbusmenu_menuitem_property_set (calendar, DBUSMENU_MENUITEM_PROP_TYPE, DBUSMENU_CALENDAR_MENUITEM_TYPE);
-		/* insensitive until we check for available apps */
-		dbusmenu_menuitem_property_set_bool(calendar, DBUSMENU_MENUITEM_PROP_ENABLED, FALSE);
-		g_signal_connect (G_OBJECT(calendar), DBUSMENU_MENUITEM_SIGNAL_ITEM_ACTIVATED,
-						  G_CALLBACK (activate_cb), "evolution -c calendar");
-		dbusmenu_menuitem_child_append(root, calendar);
 
-		g_idle_add(check_for_calendar, NULL);
+		// Always enable the calendar even if it does nothing
+		dbusmenu_menuitem_property_set_bool(calendar, DBUSMENU_MENUITEM_PROP_ENABLED, TRUE);
+		dbusmenu_menuitem_property_set_bool(calendar, DBUSMENU_MENUITEM_PROP_VISIBLE, TRUE);
+
+		dbusmenu_menuitem_child_append(root, calendar);
 	}
 
 	DbusmenuMenuitem * separator = dbusmenu_menuitem_new();
